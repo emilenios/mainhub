710c04da575e20985aee814643a53cac8bab4d3a
emilenios <58391535+emilenios@users.noreply.github.com>
2019-12-02 20:23:34 -0300

    Add files via upload
    src for vue demo of todo aplication

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    
+  </div>
+</template>
+
+<script>
+
+import Header from './components/layout/Header';
+export default {
+  name:"app",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #f2faf6;
+}
+
+#nav a.router-link-exact-active {
+  color: #9ef5ce;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about" >
+    
+      <ul >
+          <li class="list a"> LINKS <li/>
+          <li class="list"> GuitHub :https://github.com/emilenios/mainhub </li>
+          <li class="list"> Linkedin :https://www.linkedin.com/in/emiliano-salina-martinez-7105b6167</li> 
+          <li class="list"> Email: emilianosalina20@gmail.com</li>      
+      </ul>
+     
+        
+    
+  </div>
+</template>
+
+<style scoped>
+  .about{
+    text-align: center;
+    padding-left: 30px;
+    display: flex;
+    background: #fff;
+    color: rgb(2, 2, 2);
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 25px;
+  }
+
+  .list{
+    text-align: center;
+    padding-left: 5px;
+  }
+
+  .list a{
+    font-family: cursive;
+    font-size: 30px;
+  }
+</style>
